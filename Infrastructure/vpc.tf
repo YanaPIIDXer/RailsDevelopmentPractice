bf771d967f56530fd31f4a048ce07831c557fd99
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-11 06:13:45 +0900

    ロードバランサの構成を記述

--- Infrastructure/vpc.tf
+++ Infrastructure/vpc.tf
@@ -5,11 +5,21 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_subnet" "public" {
+resource "aws_subnet" "public1" {
   vpc_id = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
+  availability_zone = "ap-northeast-1a"
   tags = {
-    Name = "RailsTest_Public"
+    Name = "RailsTest_Public1"
+  }
+}
+
+resource "aws_subnet" "public2" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "ap-northeast-1c"
+  tags = {
+    Name = "RailsTest_Public2"
   }
 }
 
@@ -28,7 +38,12 @@ resource "aws_route_table" "public" {
   }
 }
 
-resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public.id
+resource "aws_route_table_association" "public1" {
+  subnet_id      = aws_subnet.public1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public2" {
+  subnet_id      = aws_subnet.public2.id
   route_table_id = aws_route_table.public.id
 }
