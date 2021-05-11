57c0f57a0f4d18589a9b364c3b767c30884ef3af
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-11 18:31:49 +0900

    ELBからの通信を受け付けるようにした

--- Infrastructure/ecs.tf
+++ Infrastructure/ecs.tf
@@ -42,19 +42,12 @@ resource "aws_security_group" "ecs_instance_sg" {
     name = "RailsTest_ECSInstance"
     description = "ECS Instance"
     vpc_id = aws_vpc.main.id
-    
-    ingress {
-        from_port        = 80
-        to_port          = 80
-        protocol         = "TCP"
-        cidr_blocks      = ["0.0.0.0/0"]
-    }
 
     ingress {
-        from_port        = 32768
-        to_port          = 61000
-        protocol         = "TCP"
-        cidr_blocks      = ["0.0.0.0/0"]
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        security_groups  = [aws_security_group.lb_sg.id]
     }
 
     egress {
