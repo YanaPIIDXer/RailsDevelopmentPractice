34b32fd7f883bc0c0bfb5f75fb962565363e413c
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-11 22:23:16 +0900

    DNSを有効にした

--- Infrastructure/rds.tf
+++ Infrastructure/rds.tf
@@ -10,6 +10,7 @@ resource "aws_db_instance" "main" {
     password             = var.database_password
     parameter_group_name = "default.mysql5.7"
     skip_final_snapshot  = true
+    publicly_accessible  = true
     db_subnet_group_name = aws_db_subnet_group.db_subnet.name
     vpc_security_group_ids = [aws_security_group.db_sg.id]
 }
